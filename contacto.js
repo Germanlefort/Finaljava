c99edc876d322aadd67eb0fbf23c14df2544818a
LeFortgerman <lefortgerman@hotmail.com>
2022-07-31 21:51:30 -0300

    subida del desafio

---
+++ contacto.js
@@ -0,0 +1,84 @@
+swal.fire("Bienvenidos!!! ")
+
+
+
+
+
+const formulario = document.querySelector("#my-form")
+const inputNombre = document.querySelector("#input-nombre")
+const inputDirrecion = document.querySelector("#input-direccion")
+const inputLocalidad = document.querySelector("#input-localidad")
+const btnEviar = document.querySelector("#btn-enviar")
+
+
+ btnEviar.addEventListener("click", () =>{
+  console.log(inputNombre.value)
+  console.log(inputDirrecion.value)
+  console.log(inputLocalidad.value)
+})
+
+formulario.addEventListener("submit", (event) => {
+ event.preventDefault()
+  console.log(inputNombre.value)
+  console.log(inputDirrecion.value)
+  console.log(inputLocalidad.value)
+
+
+
+const userData = {
+  nombre : inputNombre.value,
+  direccion : inputDirrecion.value,
+  localidad: inputLocalidad.value,
+  
+}
+ console.log(userData)
+
+
+
+}) 
+
+const btn1 = document.querySelector("#boton1")
+const btn2 = document.querySelector("#boton2")
+
+const clickear = () =>{
+  console.log("Boton Clickeado")
+} 
+
+const alertar =() =>{
+  alert("Boton clikleado!")
+}
+
+btn1.addEventListener("click", (event) => {
+  
+   swal.fire("Adelante, disfruta de nuestra tienda ")
+
+   console.log(event.target)
+  
+ })
+ 
+btn2.addEventListener("click", (event) => {
+  
+  
+swal.fire("Prohíbido la venta, expendio o suministro de Bebidas Alcoholicas a menores de edad ")
+
+   console.log(event.target)
+ 
+ })
+
+function guardar () {
+  var nuevoRegistro = []
+  nombre = '',
+  direccion= '',
+  localidad= '';
+   
+
+
+  nombre = document.getElementById("input-nombre").value;
+  direccion = document.getElementById("input-direccion").value;
+  localidad = document.getElementById("input-localidad").value;
+
+
+  localStorage.setItem("nombre", JSON.stringify(nombre));
+  localStorage.setItem("direccion", JSON.stringify(direccion));
+  localStorage.setItem("localidad", JSON.stringify(localidad));
+}
---
+++ productos.js
@@ -0,0 +1,15 @@
+let productos = [
+  { id:1, nombre: "OSO ", tipo:"Cerveza", precio: 995, Img:" ./img/oso.png"},
+  { id:2, nombre: "AMBER ", tipo:"Cerveza", precio: 859, Img:"./img/amber.png"},
+  { id:3, nombre: "CORONA ", tipo:"Cerveza", precio: 1000, Img:"./img/corona.png"},
+  { id:4, nombre: "KUNE ", tipo:"Cerveza", precio: 990, Img:"./img/kune.png"},
+  { id:5, nombre: "2407 ", tipo:"Cerveza", precio: 1050, Img:"./img/2407.png"},
+  { id:6, nombre: "ANDES ", tipo:"Cerveza", precio: 895, Img:"./img/Andes.png"},
+  { id:7, nombre: "Fernet ", tipo:"Fernet ", precio: 4500, Img:"./img/Fernet.png"},
+  { id:8, nombre: "Whisky", tipo:"Bebida Blanca", precio: 7900, Img:"./img/whisky.png"},
+  { id:9, nombre: "Gaseosas ", tipo:"Gaseosas", precio: 560, Img:"./img/cocacola.png"},
+  { id:10, nombre: "Aguas ", tipo:"Aguas Saborizadas", precio: 400, Img:"./img/levite.png"},
+  { id:11, nombre: "Sidra ", tipo:"Sidra", precio: 400, Img:"./img/sidra.png"},
+  { id:12, nombre: "Vinos ", tipo:"Vinos", precio: 2500, Img:"./img/vinos.png"},
+ ]
+ 
